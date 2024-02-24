92667c41ec3267b545514e675c5351290aa81815
tanitanin <tajima2210@gmail.com>
2024-02-25 04:22:23 +0900

    Add double_arg function and test

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,56 @@ struct Cli {
     uri: String,
 }
 
+#[derive(Debug)]
+enum CliError {
+    NotEnoughArgs,
+    Parse(std::num::ParseIntError),
+}
+impl std::fmt::Display for CliError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            CliError::NotEnoughArgs => write!(f, "Not enough arguments."),
+            CliError::Parse(ref e) => write!(f, "Parse error: {}", e),
+        }
+    }
+}
+impl std::error::Error for CliError {
+    fn description(&self) -> &str {
+        match *self {
+            CliError::NotEnoughArgs => "Not enough arguments.",
+            CliError::Parse(ref e) => e.description(),
+        }
+    }
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        None
+    }
+}
+impl From<CliError> for Error {
+    fn from(value: CliError) -> Self {
+        match value {
+            CliError::NotEnoughArgs => Error::new(HRESULT(-1), From::from("Not enough arguments.")),
+            CliError::Parse(e) => Error::new(HRESULT(-2), From::from(e.to_string())),
+        }
+    }
+}
+
+fn double_arg(arg: &str) -> std::result::Result<i32, CliError> {
+    let n = arg.parse::<i32>().map_err(|e| CliError::Parse(e))?;
+    Ok(2 * n)
+}
+
+#[test]
+fn check_double_arg_validity() {
+    assert_eq!(double_arg("100").unwrap(), 200);
+}
+
 fn main() -> Result<(), > {
     println!("{:?}", std::env::args());
     let args = Cli::parse();
+
+    let val = "1234321";
+    let double_val = double_arg("1234321")?;
+    println!("{} * 2 = {}", val, double_val);
 
     let hs_arg_uri = HSTRING::from(args.uri);
     let _uri = Uri::CreateUri(&hs_arg_uri)?;
